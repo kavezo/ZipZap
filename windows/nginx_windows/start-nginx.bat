30cff06fafe76b29045e57f958ab6098c66166fb
SuzukaGozen <73313995+SuzukaGozen@users.noreply.github.com>
2020-11-03 22:11:56 +0100

    Run nginx in foreground

--- windows/nginx_windows/start-nginx.bat
+++ windows/nginx_windows/start-nginx.bat
@@ -1,13 +1,5 @@
 @echo off
 pushd %~dp0\nginx
-
-if exist logs\nginx.pid goto :running
-start nginx
-goto :exit
-
-:running
-echo Looks like nginx is already running! You can close this window.
-pause > NUL
-
-:exit
+nginx -v
+nginx
 popd
