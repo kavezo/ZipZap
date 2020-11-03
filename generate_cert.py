85575deda6999c3c0d1e4c997a0a659a0777fc1e
kavezo <threethirtyseven111@gmail.com>
2020-11-02 22:29:04 -0700

    Fix restructure, and add cert generation

---
+++ generate_cert.py
@@ -0,0 +1,82 @@
+import datetime
+
+from cryptography import x509
+from cryptography.x509.oid import NameOID
+from cryptography.hazmat.primitives import hashes
+from cryptography.hazmat.backends import default_backend
+from cryptography.hazmat.primitives import serialization
+from cryptography.hazmat.primitives.asymmetric import rsa
+
+# From https://stackoverflow.com/a/56292132
+
+root_key = rsa.generate_private_key(
+    public_exponent=65537,
+    key_size=2048,
+    backend=default_backend()
+)
+subject = issuer = x509.Name([
+    x509.NameAttribute(NameOID.COUNTRY_NAME, u"US"),
+    x509.NameAttribute(NameOID.STATE_OR_PROVINCE_NAME, u"NA"),
+    x509.NameAttribute(NameOID.LOCALITY_NAME, u"NA"),
+    x509.NameAttribute(NameOID.ORGANIZATION_NAME, u"NA"),
+    x509.NameAttribute(NameOID.COMMON_NAME, u"MagiReCA"),
+])
+root_cert = x509.CertificateBuilder().subject_name(
+    subject
+).issuer_name(
+    issuer
+).public_key(
+    root_key.public_key()
+).serial_number(
+    x509.random_serial_number()
+).not_valid_before(
+    datetime.datetime.utcnow()
+).not_valid_after(
+    datetime.datetime.utcnow() + datetime.timedelta(days=3650)
+).sign(root_key, hashes.SHA256(), default_backend())
+
+# Now we want to generate a cert from that root
+cert_key = rsa.generate_private_key(
+    public_exponent=65537,
+    key_size=2048,
+    backend=default_backend()
+)
+new_subject = x509.Name([
+    x509.NameAttribute(NameOID.COUNTRY_NAME, u"US"),
+    x509.NameAttribute(NameOID.STATE_OR_PROVINCE_NAME, u"NA"),
+    x509.NameAttribute(NameOID.LOCALITY_NAME, u"NA"),
+    x509.NameAttribute(NameOID.ORGANIZATION_NAME, u"NA"),
+    x509.NameAttribute(NameOID.COMMON_NAME, u"*.magica-us.com")
+])
+cert = x509.CertificateBuilder().subject_name(
+    new_subject
+).issuer_name(
+    root_cert.issuer
+).public_key(
+    cert_key.public_key()
+).serial_number(
+    x509.random_serial_number()
+).not_valid_before(
+    datetime.datetime.utcnow()
+).not_valid_after(
+datetime.datetime.utcnow() + datetime.timedelta(days=2*365)
+).add_extension(
+    x509.SubjectAlternativeName([
+        x509.DNSName(u"*.magica-us.com"),
+        x509.DNSName(u"magica-us.com"),
+        x509.DNSName(u"*.xn--80axfjoj.xn--p1ai"),
+        x509.DNSName(u"xn--80axfjoj.xn--p1ai")
+    ]),
+    critical=False,
+).sign(root_key, hashes.SHA256(), default_backend())
+
+path = 'windows/nginx_windows/nginx/conf/cert'
+
+with open(path + '/ca.crt', 'bw+') as f:
+    f.write(root_cert.public_bytes(encoding=serialization.Encoding.PEM))
+with open(path + '/site.key', 'bw+') as f:
+    f.write(cert_key.private_bytes(encoding=serialization.Encoding.PEM, 
+        format=serialization.PrivateFormat.TraditionalOpenSSL,
+        encryption_algorithm=serialization.NoEncryption()))
+with open(path + '/site.crt', 'bw+') as f:
+    f.write(cert.public_bytes(encoding=serialization.Encoding.PEM))
--- gevent_server.py
+++ gevent_server.py
@@ -1,5 +1,12 @@
 from gevent.pywsgi import WSGIServer
 from app import app
 
-http_server = WSGIServer(('127.0.0.1', 5000), app)
-http_server.serve_forever()
+from multiprocessing import Process, freeze_support
+from dnserver import startDNS
+
+if __name__ == '__main__':
+    freeze_support()
+    dnsprocess = Process(target=startDNS)
+    dnsprocess.start()
+    http_server = WSGIServer(('127.0.0.1', 5000), app)
+    http_server.serve_forever()
