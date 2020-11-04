ab13dfa2699646f210b82f4104ec4c26f372442a
kavezo <threethirtyseven111@gmail.com>
2020-11-03 21:37:08 -0700

    Editing message for DNS and grammar

--- showIpAddress.bat
+++ showIpAddress.bat
@@ -4,10 +4,9 @@ echo.
 netsh interface ip show address | findstr "IP Address"
 echo. 
 echo.
-echo Now you should configure a proxy with address like
-echo the first one showed above (Usually is the first. 
-echo looks like 192.168.1.xxx)
-echo and port 8080 on your Magia Record device
+echo Now you should configure DNS settings with the first
+echo address shown above (Usually it's the first. 
+echo It should look like 192.168.1.xxx.)
 echo.
 echo.
 
