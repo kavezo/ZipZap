e1a413e932d96ffcc78fb082c5d024ae82bb277a
SuzukaGozen <73313995+SuzukaGozen@users.noreply.github.com>
2020-11-05 22:54:40 +0100

    Improvments to startup scripts (#21)
    * Better start scripts, more logging
    
    * Better setup

--- gevent_server.py
+++ gevent_server.py
@@ -9,4 +9,5 @@ if __name__ == '__main__':
     dnsprocess = Process(target=startDNS)
     dnsprocess.start()
     http_server = WSGIServer(('127.0.0.1', 5000), app)
+    print("App server started")
     http_server.serve_forever()
