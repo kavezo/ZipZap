51c09cd2f7ffee90c287c30592057683553f27f9
kavezo <threethirtyseven111@gmail.com>
2020-11-21 22:27:17 -0700

    Adding quest resume, some missing MSS

--- api/money.py
+++ api/money.py
@@ -3,11 +3,23 @@ import flask
 
 from util import dataUtil
 
+def process():
+    # get gems
+    response = {
+        "resultCode": "success",
+        "userItemList": [dataUtil.getUserObject('userItemList', 'PRESENTED_MONEY')]
+    }
+
+    money = dataUtil.getUserObject('userItemList', 'MONEY')
+    if money is not None:
+        response['userItemList'].append(money)
+    return flask.jsonify(response)
+
 # wow bold of you to let me handle money
 def handleMoney(endpoint):
     if endpoint.startswith('shop/list'):
         return flask.jsonify({'userCommonMoneyList': dataUtil.readJson('data/commonMoneyList.json'), 'resultCode': 'success'})
     elif endpoint.startswith('process'):
-        return '{}'
+        return process()
     else:
         flask.abort(501, description='Not implemented')
